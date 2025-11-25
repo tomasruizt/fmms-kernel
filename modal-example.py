901f5e29c7b2c66030cdc43edb2f6ede97dcff31
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-11-25 16:40:53 -0600

    Defer modal imports in example

--- modal-example.py
+++ modal-example.py
@@ -1,12 +1,13 @@
 import modal
-import torch
 
 app = modal.App("example-get-started")
 image = modal.Image.from_registry("pytorch/pytorch:2.9.1-cuda12.8-cudnn9-devel")
 
 
-@app.function(gpu="H100", image=image)
 def square(inner_dim: int):
+    import torch
+    import triton
+
     device = torch.device("cuda")
     a = torch.randn(1000, inner_dim, device=device, dtype=torch.bfloat16)
     b = torch.randn(inner_dim, 1000, device=device, dtype=torch.bfloat16)
@@ -14,6 +15,11 @@ def square(inner_dim: int):
     print(c.shape)
 
 
+@app.function(gpu="H100", image=image)
+def modal_square(inner_dim: int):
+    return square(inner_dim)
+
+
 @app.local_entrypoint()
 def main():
-    square.remote(inner_dim=200)
+    modal_square.remote(inner_dim=200)
