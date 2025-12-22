7f22d11bea5c3dc54892a7f9eb2aab4a0addc9a8
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-12-22 02:49:48 +0100

    Play with helion matmul and helion fused-matmul-sample

--- src/fused_mm_sampling/helion_impl.py
+++ src/fused_mm_sampling/helion_impl.py
@@ -11,7 +11,7 @@ import torch
 
 
 @helion.kernel(autotune_effort="none")
-def fused_sample_helion(weights: torch.Tensor, hidden_states: torch.Tensor) -> torch.Tensor:
+def fused_sample_helion_ckpt(weights: torch.Tensor, hidden_states: torch.Tensor) -> torch.Tensor:
     assert weights.size(1) == hidden_states.size(0)
     V, D = weights.size()  # noqa: N806
     n_hidden_states = hidden_states.size(1)
@@ -43,7 +43,91 @@ def fused_sample_helion(weights: torch.Tensor, hidden_states: torch.Tensor) -> t
     return gumbel_max, gumbel_max_idx
 
 
+def fused_sample_helion(hidden_states: torch.Tensor, weights: torch.Tensor) -> torch.Tensor:
+    maxs, max_idxs = fused_sample_helion_first_stage(hidden_states, weights)
+    # second stage: reduction
+    return max_idxs[maxs.argmax(dim=0)]
+
+
+# @helion.kernel(
+#     # static_shapes=True gives a performance boost for matmuls
+#     static_shapes=True,
+#     # Disable autotung over unrolling/range_num_stages
+#     # tl.dot is pipelined with num_stages
+#     autotune_config_overrides={
+#         "range_unroll_factors": [0, 0],
+#         "range_num_stages": [0, 0],
+#     },
+# )
+@helion.kernel(autotune_effort="none")
+def fused_sample_helion_first_stage(
+    hidden_states: torch.Tensor, weights: torch.Tensor
+) -> torch.Tensor:
+    """
+    Performs matrix multiplication of x and y with an optional epilogue function.
+    Args:
+        x (Tensor): Left matrix of shape [m, k].
+        y (Tensor): Right matrix of shape [k, n].
+        epilogue (Callable, optional): Function applied to the accumulator and tile indices
+            after the matmul. Defaults to identity (no change).
+    Returns:
+        Tensor: Resulting matrix of shape [m, n].
+    """
+    m, k = hidden_states.size()
+    k2, n = weights.size()
+    assert k == k2, f"size mismatch {k} != {k2}"
+    maxs = torch.empty(
+        [m, 1],
+        dtype=torch.promote_types(hidden_states.dtype, weights.dtype),
+        device=hidden_states.device,
+    )
+    max_idxs = torch.empty([m, 1], dtype=torch.int32, device=hidden_states.device)
+
+    for tile_m, tile_n in hl.tile([m, n]):
+        acc = hl.zeros([tile_m, tile_n], dtype=torch.float32)
+        for tile_k in hl.tile(k):
+            acc = torch.addmm(acc, hidden_states[tile_m, tile_k], weights[tile_k, tile_n])
+        acc += -(-torch.rand_like(acc, dtype=torch.float32).log()).log()
+        maxs[tile_m, 0] = hl.reduce(torch.max, acc, dim=1)
+        # this line below still fails
+        max_idxs[tile_m, 0] = acc.argmax(dim=1) + tile_n.begin
+
+    return maxs, max_idxs
+
+
+@helion.kernel(
+    # static_shapes=True gives a performance boost for matmuls
+    static_shapes=True,
+    # Disable autotung over unrolling/range_num_stages
+    # tl.dot is pipelined with num_stages
+    autotune_config_overrides={
+        "range_unroll_factors": [0, 0],
+        "range_num_stages": [0, 0],
+    },
+)
+def matmul(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """
+    Performs matrix multiplication of x and y with an optional epilogue function.
+    Args:
+        x (Tensor): Left matrix of shape [m, k].
+        y (Tensor): Right matrix of shape [k, n].
+    Returns:
+        Tensor: Resulting matrix of shape [m, n].
+    """
+    m, k = x.size()
+    k2, n = y.size()
+    assert k == k2, f"size mismatch {k} != {k2}"
+    out = torch.empty([m, n], dtype=torch.promote_types(x.dtype, y.dtype), device=x.device)
+    for tile_m, tile_n in hl.tile([m, n]):
+        acc = hl.zeros([tile_m, tile_n], dtype=torch.float32)
+        for tile_k in hl.tile(k):
+            acc = torch.addmm(acc, x[tile_m, tile_k], y[tile_k, tile_n])
+        out[tile_m, tile_n] = acc
+    return out
+
+
 if __name__ == "__main__":
+    device = torch.device("cuda")
     # TODO: This is still in the old format. Not yet migrated to
     # logits with shape [n_hidden_states, V].
     vocab_size = 100  # V
@@ -68,7 +152,7 @@ if __name__ == "__main__":
     assert torch.allclose(weights @ hidden_states, logits.T)
 
     # To bfloat 16
-    weights = weights.bfloat16()
-    hidden_states = hidden_states.bfloat16()
+    weights = weights.bfloat16().to(device)
+    hidden_states = hidden_states.bfloat16().to(device)
 
-    fused_sample_helion(weights, hidden_states)
+    result = fused_sample_helion(weights, hidden_states)
