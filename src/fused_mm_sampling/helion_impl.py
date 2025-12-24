4546ffc0803548a8bb5c28f6ac438a98447d007a
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-12-24 07:55:11 +0100

    Update Helion impl

--- src/fused_mm_sampling/helion_impl.py
+++ src/fused_mm_sampling/helion_impl.py
@@ -5,6 +5,11 @@ This module is separate from core to avoid making helion a required dependency.
 It is only used for performance comparisons.
 """
 
+import os
+
+# Since the helion kernel samples (is stochastic), we should not verify exactness.
+os.environ["HELION_AUTOTUNE_ACCURACY_CHECK"] = "0"
+
 import helion
 import helion.language as hl
 import torch
@@ -43,10 +48,21 @@ def fused_sample_helion_ckpt(weights: torch.Tensor, hidden_states: torch.Tensor)
     return gumbel_max, gumbel_max_idx
 
 
-def fused_sample_helion(hidden_states: torch.Tensor, weights: torch.Tensor) -> torch.Tensor:
-    maxs, max_idxs = fused_sample_helion_first_stage(hidden_states, weights)
+def fused_sample_helion(
+    hidden_states: torch.Tensor,  # [H, D]
+    weights: torch.Tensor,  # [V, D]
+    temperature: float,
+    num_samples: int,
+) -> torch.Tensor:
+    maxs, max_idxs, noise = fused_sample_helion_first_stage(
+        hidden_states, weights, temperature, num_samples
+    )
+    # So far, the noise is always the same. It seems the torch.rand() call is not evaluated multiple times.
+    # See: https://github.com/pytorch/helion/issues/1309
     # second stage: reduction
-    return max_idxs[maxs.argmax(dim=0)]
+    idxs = maxs[:, : weights.shape[0], :].argmax(dim=1)
+    samples = max_idxs.gather(dim=1, index=idxs[None, :])
+    return samples.squeeze(0)  # [H, num_samples]
 
 
 # @helion.kernel(
@@ -61,7 +77,10 @@ def fused_sample_helion(hidden_states: torch.Tensor, weights: torch.Tensor) -> t
 # )
 @helion.kernel(autotune_effort="none")
 def fused_sample_helion_first_stage(
-    hidden_states: torch.Tensor, weights: torch.Tensor
+    hidden_states: torch.Tensor,  # [H, D]
+    weights: torch.Tensor,  # [V, D]
+    temperature: float,
+    num_samples: int,
 ) -> torch.Tensor:
     """
     Performs matrix multiplication of x and y with an optional epilogue function.
@@ -73,26 +92,40 @@ def fused_sample_helion_first_stage(
     Returns:
         Tensor: Resulting matrix of shape [m, n].
     """
-    m, k = hidden_states.size()
-    k2, n = weights.size()
+    m, k = hidden_states.shape
+    n, k2 = weights.shape
+    bsz_n = 128
     assert k == k2, f"size mismatch {k} != {k2}"
-    maxs = torch.empty(
-        [m, 1],
+    maxs = torch.full(
+        [num_samples, helion.cdiv(n, bsz_n), m],
+        fill_value=float("-inf"),
         dtype=torch.promote_types(hidden_states.dtype, weights.dtype),
         device=hidden_states.device,
     )
-    max_idxs = torch.empty([m, 1], dtype=torch.int32, device=hidden_states.device)
+    max_idxs = torch.full(
+        maxs.shape,
+        fill_value=-1,
+        dtype=torch.long,
+        device=maxs.device,
+    )
+    noise = torch.empty([num_samples, m, n], dtype=weights.dtype, device=weights.device)
+    g_cuda = torch.Generator(device="cuda")
 
     for tile_m, tile_n in hl.tile([m, n]):
         acc = hl.zeros([tile_m, tile_n], dtype=torch.float32)
         for tile_k in hl.tile(k):
-            acc = torch.addmm(acc, hidden_states[tile_m, tile_k], weights[tile_k, tile_n])
-        acc += -(-torch.rand_like(acc, dtype=torch.float32).log()).log()
-        maxs[tile_m, 0] = hl.reduce(torch.max, acc, dim=1)
-        # this line below still fails
-        max_idxs[tile_m, 0] = acc.argmax(dim=1) + tile_n.begin
+            acc = torch.addmm(acc, hidden_states[tile_m, tile_k], weights[tile_n, tile_k].T)
+        acc /= temperature
 
-    return maxs, max_idxs
+        for sample_idx in range(num_samples):
+            unoise = torch.rand(acc.shape, dtype=acc.dtype, device=acc.device, generator=g_cuda)
+            gumbel_noise = -(-unoise.log()).log()
+            noise[sample_idx, tile_m, tile_n] = unoise
+            val = acc + gumbel_noise
+            maxs[sample_idx, tile_n.id, tile_m] = hl.reduce(torch.max, val, dim=1)
+            max_idxs[sample_idx, tile_n.id, tile_m] = val.argmax(dim=1) + tile_n.begin
+
+    return maxs, max_idxs, noise
 
 
 @helion.kernel(
@@ -128,31 +161,31 @@ def matmul(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
 
 if __name__ == "__main__":
     device = torch.device("cuda")
-    # TODO: This is still in the old format. Not yet migrated to
-    # logits with shape [n_hidden_states, V].
-    vocab_size = 100  # V
-    hidden_size = 10  # D
+    vocab_size = 128  # V
+    hidden_size = 16  # D
     logits1 = torch.arange(-vocab_size / 2, vocab_size / 2)[None, :]  # [1, V]
     logits2 = torch.arange(vocab_size / 2, -vocab_size / 2, step=-1)[None, :]  # [1, V]
-    logits = torch.cat([logits1, logits2], dim=0)  # [n_hidden_states, V]
-    n_hidden_states = logits.shape[0]
+    logits = torch.cat([logits1, logits2], dim=0)  # [H, V]
+    H = logits.shape[0]
     # use SVD to construct the hidden states that yield the logits
     # use pseudoinverse to construct the weights.
     # (there are many ways to do this, this is just one)
-    # W @ H = L.T
-    #  -> W = L.T @ H⁻¹
+    # H @ W = L
+    #  -> W = H⁻¹ @ L
     U, S, Vt = torch.linalg.svd(logits, full_matrices=False)
-    hidden_states = torch.cat(  # [D, n_hidden_states]
+    hidden_states = torch.cat(  # [D, H]
         [
             U.T,
-            torch.rand((hidden_size - n_hidden_states, n_hidden_states)),  # padding
+            torch.rand((hidden_size - H, H)),  # padding
         ],
     )
-    weights = logits.T @ torch.linalg.pinv(hidden_states)  # [V, D]
-    assert torch.allclose(weights @ hidden_states, logits.T)
+    hidden_states = hidden_states.T.contiguous()  # [H, D]
+    weights = torch.linalg.pinv(hidden_states) @ logits  # [D, V]
+    weights = weights.T.contiguous()  # [V, D]
+    assert torch.allclose(hidden_states @ weights.T, logits)
 
     # To bfloat 16
     weights = weights.bfloat16().to(device)
-    hidden_states = hidden_states.bfloat16().to(device)
+    hidden_states = hidden_states.bfloat16().to(device)  # [H, D]
 
-    result = fused_sample_helion(weights, hidden_states)
+    result = fused_sample_helion(weights, hidden_states, temperature=1.0)
