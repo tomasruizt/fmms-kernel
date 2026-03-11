c823840694504fcb60f8b0dd48cdceddae36b610
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2026-03-11 12:49:44 +0100

    Document tensor parallel usage

--- src/fused_mm_sampling/__init__.py
+++ src/fused_mm_sampling/__init__.py
@@ -5,9 +5,11 @@ and sampling operations using PyTorch and Triton.
 """
 
 from .core import fused_mm_sample_triton
+from .tp_info import TPInfo
 
 __version__ = "0.1.0"
 
 __all__ = [
+    "TPInfo",
     "fused_mm_sample_triton",
 ]
